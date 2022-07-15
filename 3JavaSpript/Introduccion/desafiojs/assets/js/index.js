096a663427cfdf84e8d51923b0294c30abf39ff9
Felipe Cruz Bonilla <felipecruzbonilla@gmail.com>
2022-07-14 23:21:00 -0400

    desafio js

--- 3JavaSpript/Introduccion/desafiojs/assets/js/index.js
+++ 3JavaSpript/Introduccion/desafiojs/assets/js/index.js
@@ -1,9 +1,23 @@
+$( document ).ready(function() {
+
 cantidad = document.querySelector('#cantidad');
 color = document.querySelector('#color');
 precio = document.querySelector('#precio');
 
 total = document.querySelector('#total');
 totalCantidad = document.querySelector('#totalCantidad')
+circulo = document.querySelector('#circulo')
+
+
+
+var botton = document.getElementById("botton");
+botton.onclick = btnactivo();
+    console.log( "ready!" );
+$('#botton').click(function(){
+        alert(this.id);
+    });
+
+});
 
 
 function btnactivo(){
@@ -11,4 +25,6 @@ function btnactivo(){
     console.log(precio.innerHTML);
     console.log(cantidad.value);
     totalCantidad.innerHTML = cantidad.value;  
+    console.log(color.value);
+    circulo.style.backgroundColor = color.value;
 }
