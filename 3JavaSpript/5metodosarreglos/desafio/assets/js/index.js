bd6f2fddd0e1d49a5d6df05a5aac3c9d9d828917
Felipe Cruz Bonilla <felipecruzbonilla@gmial.com>
2022-08-26 20:00:42 -0400

    todos

---
+++ 3JavaSpript/5metodosarreglos/desafio/assets/js/index.js
@@ -0,0 +1,71 @@
+const newTarea = document.querySelector('#newTarea');
+const btnTarea = document.querySelector('#btnTarea');
+const total = document.querySelector('#total');
+const check = document.querySelector('#check');
+const lista = document.querySelector('#lista');
+const borrar = document.querySelector('#borrar');
+const checkEstado = 'fa-check-circle';
+const uncheckEstado = 'fa-circle' 
+let tareas = [
+    { id: 1, nombre: "Crear Elemento",completado: false },
+    { id: 2, nombre: "Borrar Elemento",completado: false },
+    { id: 3, nombre: "Actualizar Elemento",completado: false }
+];
+
+ tareas.forEach(x => renderTarea());
+
+function renderTarea(){
+    let html = "";
+    for (let tarea of tareas){
+   
+        const completado = tarea.completado ?checkEstado : uncheckEstado;
+     
+        html += `<li id="elemento">
+        <p id="idTarea">${tarea.id}</p>
+        <p class="text">  ${tarea.nombre}</p>
+        <i class="far ${completado} co" data="realizado" id="borrar" onclick="checkTarea(${tarea.id});"></i>
+        <i class="fas fa-trash de" data="eliminado" onclick="borrarTarea(${tarea.id});" id="0"></i> 
+        </li> `
+    }
+    const contarRealizadas = tareas.filter((tareas) => tareas.completado === true);
+ 
+
+    check.innerHTML = contarRealizadas.length;
+    lista.innerHTML = html;
+    total.innerHTML = tareas.length;
+}
+
+btnTarea.addEventListener('click', async () =>{
+    const tarea = newTarea.value;
+    
+    const idcontar = (tareas[tareas.length-1].id)+1; 
+
+    console.log(idcontar)
+
+    tareas.push({
+        id: idcontar,
+        nombre: tarea, 
+        completado: false
+    });
+
+    newTarea.value = '';    
+
+    renderTarea();
+})
+
+function borrarTarea(id){
+    const index = tareas.findIndex(tareas => tareas.id  == id);
+    tareas.splice(index,1);
+    renderTarea();
+}
+
+function checkTarea(id){
+    const index = tareas.findIndex(tareas => tareas.id  == id);
+    if(tareas[index].completado == false ){
+        tareas[index].completado = true;
+    }else{
+        tareas[index].completado = false;
+    }
+    renderTarea();
+}
+
---
+++ 3JavaSpript/6api/desafio/assets/js/index.js
@@ -0,0 +1,84 @@
+async function conexionApi(currency) {
+    try {
+        const newLocal = "https://mindicador.cl/api/";
+        const res = await fetch(newLocal + currency);
+        const data = await res.json();
+        return data;
+    } catch (e) {
+    alert("NO se logra conectar a la a mindicador.cl");
+    }
+}
+
+
+let btn_search = document.querySelector("#btn_buscar");
+let result = document.querySelector('#resultado');
+
+
+
+
+btn_search.addEventListener("click", async ()  =>  {
+    const clpMonto = parseFloat(document.querySelector("#clpMonto").value);
+    const currency = document.querySelector("#selector_moneda").value;
+
+    let resultado = await conexionApi(currency);
+    let valorMoneda = parseFloat(resultado.serie[0].valor);
+    let calculo = clpMonto/valorMoneda;
+
+
+    result.innerHTML = 'Resultado: ' + calculo;
+
+    var xmlhttp = new XMLHttpRequest();
+    var url = "https://mindicador.cl/api/" + currency;
+    xmlhttp.open("GET",url,true);
+    xmlhttp.send();
+    xmlhttp.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+        let data = [];
+        for(var i=0; i < 10; i++){
+            data[i] = resultado.serie[i];
+          }
+          
+         console.log(data)
+            date = data.map(function(elem){
+                return elem.fecha;
+            })
+            valorDia = data.map(function(elem){
+                return elem.valor;
+            })
+
+            const ctx = document.getElementById('grafica').getContext('2d');
+            const myChart = new Chart(ctx, {
+                type: 'line',
+                data: {
+                    labels: date,
+                    datasets: [{
+                        label: currency,
+                        data: valorDia,
+                        backgroundColor: "#fff"
+
+                    }]
+                },
+                options: {
+                    scales: {
+                        xAxes: [
+
+                            {
+                              // aqui controlas la cantidad de elementos en el eje horizontal con autoSkip
+                                 ticks: {
+                       
+                                    stepSize: 10,
+                                   
+                                }
+                            }
+                          ],
+                        y: {
+
+                            beginAtZero: true
+                        }
+                    }
+                }
+            });
+        }
+
+    }
+})
